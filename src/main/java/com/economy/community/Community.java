83b01e83eb089c6645825c3fd26815789371a7e7
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-07 14:28:58 +0900

    feat: community Entity

--- src/main/java/com/economy/community/Community.java
+++ src/main/java/com/economy/community/Community.java
@@ -2,19 +2,22 @@ package com.economy.community;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Community {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private long userId;
     private String userNickname;
