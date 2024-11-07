0a222e06971b9ed120ec48fe0f4ede27933ed271
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-07 16:45:48 +0900

    feat: community controller

---
+++ src/main/java/com/economy/community/CommunityController.java
@@ -0,0 +1,12 @@
+package com.economy.community;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/communities")
+@RequiredArgsConstructor
+public class CommunityController {
+    
+}
