e5d36f6ea863ff2553557724605be29a3c43633c
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-08 10:35:10 +0900

    feat: community controller

--- src/main/java/com/economy/community/CommunityController.java
+++ src/main/java/com/economy/community/CommunityController.java
@@ -2,8 +2,12 @@ package com.economy.community;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CommunityController {
 
-    private final CommunityServiceImpl service;
+    private final CommunityService service;
 
     @GetMapping
     public List<Community> getCommunities() {
@@ -22,5 +26,20 @@ public class CommunityController {
     @GetMapping("/{id}")
     public Community getCommunity(@PathVariable Long id) {
         return service.getCommunityById(id);
+    }
+
+    @PostMapping
+    public Community createCommunity(@RequestBody CommunityRequest request) {
+
+    }
+
+    @PutMapping("/{id}")
+    public Community updateCommunity(@RequestBody CommunityRequest request, @PathVariable Long id) {
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCommunity(@PathVariable Long id) {
+
     }
 }
