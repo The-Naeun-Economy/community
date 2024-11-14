8f17c1c24713fbf25d564c0961fb618c94041523
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-14 17:34:51 +0900

    feat: CreatePostResponse DTO 생성

--- src/main/java/com/economy/community/dto/CreatePostResponse.java
+++ src/main/java/com/economy/community/dto/CreatePostResponse.java
@@ -1,4 +1,18 @@
 package com.economy.community.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CreatePostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    private String userNickname;
+    private String category;
+    private Long likesCount;
+    private Long viewCount;
+    private Long commentsCount;
 }
