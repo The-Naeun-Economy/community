aa5965fbb0f73dbcf5a158734caf80d24e7ece46
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-14 17:37:02 +0900

    feat: UpdatePostResponse DTO 생성: category 수정은 불가하므로 category 필드 제거

--- src/main/java/com/economy/community/dto/UpdatePostResponse.java
+++ src/main/java/com/economy/community/dto/UpdatePostResponse.java
@@ -1,4 +1,17 @@
 package com.economy.community.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UpdatePostResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private Long userId;
+    private String userNickname;
+    private Long likesCount;
+    private Long viewCount;
+    private Long commentsCount;
 }
