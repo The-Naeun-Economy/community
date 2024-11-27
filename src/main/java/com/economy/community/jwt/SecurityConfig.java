b0d7d1f7c86b719e3d43d9149c036422c5aaf481
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-27 14:19:41 +0900

    test

--- src/main/java/com/economy/community/jwt/SecurityConfig.java
+++ src/main/java/com/economy/community/jwt/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/posts").permitAll()
+                        
                         .requestMatchers("/posts/**").authenticated() // 인증 필요 경로
                         .anyRequest().permitAll()
                 )
