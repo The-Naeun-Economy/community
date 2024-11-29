d6b04294494c54788e9c3816a6ae6ccff92c3c1d
llcodingll <ll.o.llwhkr@gmail.com>
2024-11-29 12:21:36 +0900

    fix: 엔드포인드에 버전 명시

--- src/main/java/com/economy/community/controller/PostController.java
+++ src/main/java/com/economy/community/controller/PostController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/posts")
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostController {
 
