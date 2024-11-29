75144215eb5d88210a89c76f3bb67a5d04170e8c
JoGyoungJun <yoop80075@gmail.com>
2024-11-29 17:49:26 +0900

    시큐어 수정

--- src/main/java/com/economy/community/controller/PostController.java
+++ src/main/java/com/economy/community/controller/PostController.java
@@ -1,33 +1,17 @@
 package com.economy.community.controller;
 
-import com.economy.community.dto.CreatePostRequest;
-import com.economy.community.dto.CreatePostResponse;
-import com.economy.community.dto.PostLikesResponse;
-import com.economy.community.dto.PostResponse;
-import com.economy.community.dto.UpdatePostRequest;
-import com.economy.community.dto.UpdatePostResponse;
+import com.economy.community.dto.*;
 import com.economy.community.jwt.TokenProvider;
 import com.economy.community.service.PostService;
-import jakarta.persistence.Table;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@Table
+import java.util.List;
+
+
 @CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -42,7 +26,7 @@ public class PostController {
             @RequestParam(required = false) String category,
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "10") int size) {
-
+        System.out.print("run");
         return service.getPosts(category, page, size);
     }
 
